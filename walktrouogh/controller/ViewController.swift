f3cca2c0a15308283f422bfbe279551d2467e482
Mohamed Alouane <flahweb@gmail.com>
2018-11-03 09:41:06 -0700

    fix swipe gesture with page controller

--- walktrouogh/controller/ViewController.swift
+++ walktrouogh/controller/ViewController.swift
@@ -15,52 +15,8 @@ extension UIColor{
 class ViewController: UIViewController {
     
     
-    //Make sure you do encapsulation
-    private let previousButton : UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("Prev", for: .normal)
-        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        return btn
-    }()
-    
-    private let nextButton : UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("Next", for: .normal)
-        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        btn.setTitleColor(.gray, for: .normal)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.setTitleColor(.mainRed, for: .normal)
-        return btn
-    }()
-    
-        private let pageControle : UIPageControl  = {
-        let pc = UIPageControl()
-        pc.currentPage = 0
-        pc.numberOfPages = 4
-        pc.currentPageIndicatorTintColor = .mainRed
-        pc.pageIndicatorTintColor = .gray
-        return pc
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupButtonControls()
-    }
-    
-    fileprivate func setupButtonControls(){
-        let yellowV = UIView()
-        yellowV.backgroundColor = .yellow
-        
-        let bottomStackView = UIStackView(arrangedSubviews: [previousButton, pageControle, nextButton])
-        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
-        bottomStackView.distribution = .fillEqually
-        view.addSubview(bottomStackView)
-        NSLayoutConstraint.activate([bottomStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                                     bottomStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-             bottomStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-             bottomStackView.heightAnchor.constraint(equalToConstant: 50)
-                                     ])
     }
 }
 
